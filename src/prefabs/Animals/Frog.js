ff0dc8e3813095a396a3685a2d220595217a3b6a
Sean Carpenter <selcarpe@ucsc.edu>
2020-07-23 10:51:32 -0700

    physics changes

--- src/prefabs/Animals/Frog.js
+++ src/prefabs/Animals/Frog.js
@@ -16,21 +16,15 @@ class Frog extends Phaser.Physics.Arcade.Sprite{
         //Set ground accelleration
         if (this.onGround){
             if(keyLEFT.isDown){
-                if(this.body.velocity.x>0){
-                    this.setDragX(20);
-                }
                 this.setAccelerationX(-100);
             }
             else if (keyRIGHT.isDown){
-                if(this.body.velocity.x<0){
-                    this.setDragX(20);
-                }
                 this.setAccelerationX(100);
             }
             //Set drag on ground when not holding left or right
             else{
                 this.setAccelerationX(0);
-                this.setDragX(100);
+                this.setDragX(75);
             }
         }
         //Set air movement
@@ -44,11 +38,11 @@ class Frog extends Phaser.Physics.Arcade.Sprite{
             //set drag in air when not holding left or right
             else{
                 this.setAccelerationX(0);
-                this.setDragX(20);
+                this.setDragX(30);
             }
         }
         //Jump when pressing space
-        if(Phaser.Input.Keyboard.JustDown(keySPACE) && this.onGround){
+        if(Phaser.Input.Keyboard.JustDown(keySPACE) && this.body.velocity.y <= 0 && this.onGround){
             this.onGround=false;
             this.setVelocityY(-400);
             this.sfxJump.play();
@@ -56,7 +50,7 @@ class Frog extends Phaser.Physics.Arcade.Sprite{
         }
 
     }
-    //reset inAir flag
+    //reset onGround flag
     reset(){
         this.onGround=true;
     }
--- src/scenes/Kermit.js
+++ src/scenes/Kermit.js
@@ -36,34 +36,35 @@ class Kermit extends Phaser.Scene{
             fixedWidth: 0
         }
         
+        //create tilemap
         const map = this.add.tilemap("swamp_map");
         const tileset =  map.addTilesetImage('swampAssets', 'platforms');
+        
+        //for compares in update()
         this.map=map;
+
         //set scene gravity
         this.physics.world.gravity.y = 250;
         
         //set backgound
         this.swamp = this.add.tileSprite(0,0,game.config.width,game.config.height,'swamp').setOrigin(0);
+        
+        //prep background for scroll
         this.swamp.setScrollFactor(0);
+
+        //set up tilemap layers
+        //platform tiles
         const platformTiles = map.createStaticLayer("Platforms",tileset, 0, 0);
+        
+        //spawn and goal objects
         const importantPoints= map.createStaticLayer("ImportantPoints", tileset, 0, 0);
+        
+        //set spawn point
         const spawn= map.findObject("ImportantPoints", obj => obj.name === "Spawn");
 
-
+        //give platform tiles collision
         platformTiles.setCollisionByProperty({collides: true});
 
-
-        // const debugGraphics = this.add.graphics().setAlpha(0.75);
-        // platformTiles.renderDebug(debugGraphics, {
-        //     tileColor: null, // Color of non-colliding tiles
-        //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-        //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-        // });
-        //create immovable platforms for ground
-        //this.ground = this.physics.add.staticGroup();
-        //this.ground.create(160, 430,'floor').setScale(1.5,.5).refreshBody();
-        //this.ground.create(600,150,'floor' ).setScale(1.7,.25).refreshBody();
-        //this.ground.propertyValueSet('Body.friction.x', 1);
 
         //create player character using frog instance
         this.player = new Frog(this, spawn.x, spawn.y, 'witch','Idle000');
@@ -72,45 +73,57 @@ class Kermit extends Phaser.Scene{
         this.physics.world.enable(this.player);
         this.player.setMaxVelocity(500)
         //set bounce and player collision
-        this.player.setBounce(0.2);
+        this.player.setBounce(0.1);
         this.player.body.onCollide=true;
 
 
         //set platform collision, reset inAir trigger on collision
         this.physics.add.collider(this.player, platformTiles);
-
+        //this.physics.world.setBoundsCollision(true,true,true,false);
+        this.physics.world.setBounds(0,0,this.map.widthInPixels,this.map.heightInPixels,true, true, true, false);
+        this.player.setCollideWorldBounds(true);
         //key inputs
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         
+        //set camera to follow player 
         this.cameras.main.setBounds(0,0, map.widthInPixels, map.heightInPixels);
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setLerp(.5,0);
 
-
+        //silly scene text for debug
         this.add.text(0,0, "Kermit",menuConfig).setOrigin(0);
 
     }
     update(){
+
+        //player update loop
         this.player.update();
 
-        if (this.player.body.onFloor()){
+        //collision with tiles
+        if (this.player.body.blocked.down){
             this.player.reset();
         }
 
+        //player falls off screen -> restart scene
         if (this.player.body.position.y>game.config.height){
             this.scene.restart();
         }
 
-        if (this.player.body.position.y<0){
-            this.player.body.position.y=0;
-        }
+        //player about to go off screen -> set y velocity to 0
+        // if (this.player.body.position.y<0){
+        //     this.player.setAccelerationY(0);
+        //     this.player.setVelocityY(0);
+        // }
 
-        if (this.player.body.position.x + this.player.w>this.map.widthInPixels){
-            this.player.body.position.x=this.map.widthInPixels;
-        }
+        //player about to run off right side of scene -> set x velocity to 0
+        // if (this.player.body.position.x + this.player.width>this.map.widthInPixels || this.player.body.position.x < 0){
+        //     //this.player.setAccelerationX(0);
+        //     this.player.setVelocityX(0);
+        // }
 
+        //carmera scrolls -> background scrolls proportionally
         this.swamp.tilePositionX=this.cameras.main.scrollX*.3
         
 
